0fa94e45c2f6c8406680736e69d7e268553dabfa
Jayme Holanda <112284700+JaymeHolanda@users.noreply.github.com>
2022-09-13 17:45:23 -0300

    comit

---
+++ Python/Guanabara Exercicios/ex014.py
@@ -0,0 +1,7 @@
+# PROGRAMA QUE CONVERTA TEMPERATURA DE GRAUS CELSIOS PARA FAHRENHEIT 
+
+c = float(input('Digite a temperatuda em graus Celsios aqui:     '))
+f = c * 1.8 + 32
+# F = c * 1,8 + 32
+
+print(f'A temperatura atual em Fahrenheit é de {f:.2f}F')
